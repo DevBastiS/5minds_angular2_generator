3267ac16b8d23e0d76251b702992e5342f6e8e60
bastian schnittger <bastian.schnittger@5minds.de>
2017-05-12 10:26:07 +0200

    del env from moch repo

--- generators/app/templates/module/repository.mock.ts
+++ generators/app/templates/module/repository.mock.ts
@@ -1,5 +1,4 @@
 import {Injectable} from '@angular/core';
-import {environment} from 'environment';
 import {Observable} from 'rxjs/Observable';
 import {I<%=moduleName%>Repository} from './<%=moduleFileName%>.repository.interface';
 
