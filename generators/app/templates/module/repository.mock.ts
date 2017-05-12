cdfea38deed120a4eae9d63303c890fc08d92122
bastian schnittger <bastian.schnittger@5minds.de>
2017-05-12 10:25:48 +0200

    delete http from mock repo

--- generators/app/templates/module/repository.mock.ts
+++ generators/app/templates/module/repository.mock.ts
@@ -1,5 +1,4 @@
 import {Injectable} from '@angular/core';
-import {Http} from '@angular/http';
 import {environment} from 'environment';
 import {Observable} from 'rxjs/Observable';
 import {I<%=moduleName%>Repository} from './<%=moduleFileName%>.repository.interface';
@@ -8,6 +7,6 @@ import {I<%=moduleName%>Repository} from './<%=moduleFileName%>.repository.inter
 @Injectable()
 export class <%=moduleName%>MockRepository implements I<%=moduleName%>Repository {
 
-  constructor(private http: Http) {}
+  constructor() {}
   
 }
